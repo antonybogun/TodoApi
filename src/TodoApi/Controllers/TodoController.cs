1122b66382419f5b2892db40d71286662195e0f2
antonybogun <antonybogun@gmail.com>
2016-12-09 14:37:23 -0500

    Implement CRUD

--- src/TodoApi/Controllers/TodoController.cs
+++ src/TodoApi/Controllers/TodoController.cs
@@ -45,5 +45,36 @@ namespace TodoApi.Controllers
             TodoItems.Add(item);
             return CreatedAtRoute("GetTodo", new { id = item.Key }, item);
         }
+
+        [HttpPut("{id}")]
+        public IActionResult Update(string id, [FromBody] TodoItem item)
+        {
+            if (item == null || item.Key != id)
+            {
+                return BadRequest();
+            }
+
+            var todo = TodoItems.Find(id);
+            if (todo == null)
+            {
+                return NotFound();
+            }
+
+            TodoItems.Update(item);
+            return new NoContentResult();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(string id)
+        {
+            var todo = TodoItems.Find(id);
+            if (todo == null)
+            {
+                return NotFound();
+            }
+
+            TodoItems.Remove(id);
+            return new NoContentResult();
+        }
     }
 }
