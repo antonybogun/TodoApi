9dee812637ebf9c9bee1786794dc5864c12cb73d
antonybogun <antonybogun@gmail.com>
2016-12-09 13:54:22 -0500

    Add controller layer

---
+++ src/TodoApi/Controllers/TodoController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TodoApi.Models;
+
+// For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TodoApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class TodoController : Controller
+    {
+        public TodoController(ITodoRepository todoItems)
+        {
+            TodoItems = todoItems;
+        }
+        public ITodoRepository TodoItems { get; set; }
+    }
+}
--- src/TodoApi/Startup.cs
+++ src/TodoApi/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using TodoApi.Models;
 
 namespace TodoApi
 {
@@ -29,7 +30,9 @@ namespace TodoApi
         {
             // Add framework services.
             services.AddMvc();
+            services.AddSingleton<ITodoRepository, TodoRepository>();
         }
+    }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
